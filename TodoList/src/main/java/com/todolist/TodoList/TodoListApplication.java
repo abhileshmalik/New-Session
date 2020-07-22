1da80740bf53859e2a9b2bfb61be7f72cdb8c3b3
abhilesh <abhilesh.malik@tothenew.com>
2020-07-23 03:32:16 +0530

    Todo App1

--- TodoList/src/main/java/com/todolist/TodoList/TodoListApplication.java
+++ TodoList/src/main/java/com/todolist/TodoList/TodoListApplication.java
@@ -2,10 +2,17 @@ package com.todolist.TodoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TodoListApplication {
+public class TodoListApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoListApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
